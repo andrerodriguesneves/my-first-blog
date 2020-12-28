7167a41acbe69c41857d60eb811565f90138aef0
andre.rodriguesneves <andre.rodriguesneves@gmail.com>
2020-12-27 21:51:54 -0300

    Alteração do layout

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-    """
-    TEste dois
 
-
-    """
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
         "-published_date"
     )
